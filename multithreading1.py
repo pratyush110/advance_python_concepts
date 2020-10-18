2e9263dda6d12a77aba10a0b0be0a4293efc6aa8
pratyush110 <pratyush.priyadarshi11@gmail.com>
2020-10-18 18:51:41 +0530

    using threadpool

--- multithreading1.py
+++ multithreading1.py
@@ -5,25 +5,31 @@ Created on Sun Oct 18 11:18:53 2020
 
 @author: pratyush
 """
-import threading
+#import threading
 import time
+import concurrent.futures
 
 start = time.perf_counter()
 
 def do_something(seconds):
     print(f'Sleeping {seconds} second(s)...')
     time.sleep(seconds)
-    print('done sleeping')
+    return f'Done Sleeping...{seconds}'
 
-threads=[]
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    #f1 = executor.submit(do_something, 1)
+    secs = [5, 4, 3, 2, 1]
+    results = executor.map(do_something, secs)
 
-for _ in range(10):
-    t=threading.Thread(target=do_something, args=[1.5])
-    t.start()
-    threads.append(t)
-
-for thread in threads:
-    thread.join()
+#threads=[]
+#
+#for _ in range(10):
+#    t=threading.Thread(target=do_something, args=[1.5])
+#    t.start()
+#    threads.append(t)
+#
+#for thread in threads:
+#    thread.join()
 
 finish = time.perf_counter()
 
