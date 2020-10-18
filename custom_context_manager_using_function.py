9ce64ff54e3f6273210a12c4299a6ac1021f6495
pratyush110 <pratyush.priyadarshi11@gmail.com>
2020-10-18 18:25:22 +0530

    showcasing more real world use of custom context managers

--- custom_context_manager_using_function.py
+++ custom_context_manager_using_function.py
@@ -5,8 +5,10 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import os
 from contextlib import contextmanager
 
+#custom context manager mimicing with statement for file handling
 @contextmanager
 def open_file(file, mode):
     try:
@@ -19,3 +21,18 @@ with open_file('sample.txt', 'w') as f:
     f.write('Testing')
     
 print(f.closed)
+
+#contxt manager to change directory and print the files in there and change
+#back to original directory
+#can be modified to work with db, locks, etc
+@contextmanager
+def change_dir(destination):
+    try:
+        cwd = os.getcwd()
+        os.chdir(destination)
+        yield
+    finally:
+        os.chdir(cwd)
+    
+with change_dir('dir1'):
+    print(os.listdir())
